3e0d30d4c24e3d94ec57ed1f35dce3223e594677
John <johnadepelumi@gmail.com>
2022-06-18 06:20:38 +0100

    added responsiveness to the landing page

--- src/components/Text1/Text1.js
+++ src/components/Text1/Text1.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import './Text1.css'
+import React from "react";
+import "./Text1.css";
 
 const Text1 = () => {
   return (
-    <div className='Text1'>
-    <h5>Worry no more!!!</h5>
-    <h3> <b>DIGICORD</b> Your No.1 Medical Record Bank</h3>
-    <h1>IS LAUNCHING SOON!!</h1>
-  </div>
-  )
-}
+    <div className="Text1">
+      <h5>Worry no more!!!</h5>
+      <h3>
+        <b>DIGICORD</b> Your No.1 Medical Record Bank is
+      </h3>
+      <h1>LAUNCHING SOON!!!</h1>
+    </div>
+  );
+};
 
-export default Text1
+export default Text1;
--- src/components/Text2/Text2.js
+++ src/components/Text2/Text2.js
@@ -5,7 +5,7 @@ const Text2 = () => {
   return (
     <div className='Text2'>
     Digicord is here to make your Medical Records available anyday,anytime regardless of wherever you are in the world. Isn't that Exciting!
-    <p>Subscribe to our Newsletter and social media handles to get daily update on how we intend to serve you</p>
+    {/* <p>Subscribe to our Newsletter and social media handles to get daily update on how we intend to serve you</p> */}
     </div>
     // <em>&copy;Digicord</em>
   )
